7048d5c02b17d25dc2c2a5d8efa5262555ea010d
ibrahimshamma99 <i.abushammah@gmail.com>
2020-03-29 16:44:42 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Main from './template/Main';
+// import Details from './views/Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Route exact path='/' component={Main} />
+        {/**<Route path='/:country' component={Details} />*/}
+      </Router>
     </div>
   );
 }
---
+++ src/template/Main/index.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import ReactTooltip from 'react-tooltip';
+import { getAll, getAllCountries } from '../../api';
+import WorldMap from '../../components/Map';
+import Search from '../../components/Search';
+import GlobalStats from '../../components/WorldStats';
+import { useStore } from '../../Store';
+import '../../app.css';
+
+function Main() {
+  const { state, dispatch } = useStore();
+  const [covid19Data, setCovid19Data] = useState({});
+  const [tooltipContent, setTooltipContent] = useState({});
+
+  useEffect(() => {
+    (async function getData() {
+      const data = await getAll();
+      setCovid19Data(() => data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async function getAllCountriesData() {
+      const data = await getAllCountries();
+      dispatch({ type: 'SET_COUNTRIES', payload: data });
+    })();
+  }, [dispatch]);
+
+  return (
+    <>
+      <Search initialData={state.countries.map(country => country.country)} />
+      <GlobalStats stats={covid19Data} />
+      <WorldMap
+        setTooltipContent={setTooltipContent}
+        covid19DataScale={covid19Data.cases}
+        countriesData={state.countries}
+      />
+      {tooltipContent.length !== 0 && (
+        <ReactTooltip>
+          <h4>{tooltipContent.name && tooltipContent.name}</h4>
+          {tooltipContent.cases && <h5>Cases: {tooltipContent.cases}</h5>}
+          {tooltipContent.recovered && (
+            <h5>Recovered: {tooltipContent.recovered}</h5>
+          )}
+          {tooltipContent.deaths && <h5>Deaths: {tooltipContent.deaths}</h5>}
+        </ReactTooltip>
+      )}
+      <div>
+        &#x24B8; Ibrahim Made with
+        <span style={{ color: 'red' }}> &#x2764; </span>
+        using
+        <a href='https://github.com/NovelCOVID/API'> NovelCOVID API.</a>
+      </div>
+    </>
+  );
+}
+
+export default Main;
