15d4ec517b3779d8c8543f023503043e8825557c
ibrahimshamma99 <i.abushammah@gmail.com>
2020-03-30 02:21:30 +0300

    Updates

---
+++ precache-manifest.f2a5e66584f259236f7464592e754cd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4996f29ebc1897178adb6abb71c66855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555f08c8b42a19f5917d",
+    "url": "/static/js/2.c109e519.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.c109e519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ed10d86f44b4357255",
+    "url": "/static/js/main.6addc590.chunk.js"
+  },
+  {
+    "revision": "475e629f97b572cbd433",
+    "url": "/static/js/runtime-main.82a2a924.js"
+  }
+]);
--- src/components/Map/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { ComposableMap } from 'react-simple-maps';
-
-const StyledComposableMap = styled(ComposableMap)`
-  height: 100vh;
-  width: 10vh;
-`;
-
-export { StyledComposableMap };
